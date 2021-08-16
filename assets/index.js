e19bee0eda026077e7e41179dee50ff684f6fb57
Volchenok <warp.speed@hotmail.com>
2021-08-16 15:31:03 -0700

    changes

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,29 @@
-var topicField = document.getElementById("topic");
-var countryField = document.getElementById("country");
-var searchBtn = document.getElementById("searchBtn");
-var resHead = document.getElementById("resultsHead");
-var allNewsDiv = document.getElementById("all-news-div");
+let topicField = document.getElementById("topic");
+let countryField = document.getElementById("country");
+let searchBtn = document.getElementById("searchBtn");
+let resHead = document.getElementById("resultsHead");
+let allNewsDiv = document.getElementById("all-news-div");
 
 
-searchBtn.addEventListener('click',()=>{
-    var apiUrl = "https://newsapi.letsoft.org/?apiKey=pub_81103400ec953ffe52f4620b597e20e922e&q=";
-    var apiUrl2 = "https://ipinfo.io/119.160.97.37?token=34a756c5be6b90";
-    
+searchBtn.addEventListener('click', () => {
+    let apiUrl = "https://newsapi.letsoft.org/?apiKey=pub_81103400ec953ffe52f4620b597e20e922e&q=";
+    let apiUrl2 = "https://ipinfo.io/119.160.97.37?token=34a756c5be6b90";
+
     allNewsDiv.innerHTML = "";
-    resHead.innerHTML = "Results According To "+topicField.value
+    resHead.innerHTML = "Results According To " + topicField.value
     apiUrl = apiUrl + topicField.value;
-    if(countryField.value !=""){
-        apiUrl = apiUrl +"&country="+countryField.value
+    if (countryField.value != "") {
+        apiUrl = apiUrl + "&country=" + countryField.value
     }
 
-    if(countryField.value == ""){
+    if (countryField.value == "") {
         $.ajax({
             url: apiUrl2,
             method: "get",
             dataType: 'json',
             async: false,
             success: (res) => {
-               apiUrl = apiUrl +"&country="+res['country'];
+                apiUrl = apiUrl + "&country=" + res['country'];
             },
             error: (err) => {
                 console.log(err);
@@ -32,56 +32,90 @@ searchBtn.addEventListener('click',()=>{
     }
 
     apiUrl = apiUrl + "&language=en";
-   
+
     $.ajax({
         url: apiUrl,
         method: "post",
         dataType: 'json',
         success: (res) => {
-            var htmlToAppend = "";
-           if(res['results'].length == 0){
-            resHead.innerHTML = "Results Are Not Found Related To "+topicField.value;
-           }
-            for(let i = 0; i < res['results'].length; i++){
-                console.log(res['results'][i]);
-                htmlToAppend += `
-                    <div class="news-wrapper">
-                        <h2>${res['results'][i].title}</h2>
-                        <h3>Link:</h2>
-                        <a href="${res['results'][i].link}">Click here</a>`
 
-                if(res['results'][i].image_url) {
-                    htmlToAppend += `
-                        <img src="${res['results'][i].image_url}" alt="" srcset="">
-                    `
-                }
+            if (res['results'].length == 0) {
+                apiUrl = "https://newsapi.letsoft.org/?apiKey=pub_81103400ec953ffe52f4620b597e20e922e&q=";
+                apiUrl = apiUrl + topicField.value;
+                apiUrl = apiUrl + "&language=en";
 
-                if(res['results'][i].description) {
-                    htmlToAppend += `
-                        <h3>Description:</h2>
-                        <p>${res['results'][i].description}</p>
-                    `
-                }
+                $.ajax({
+                    url: apiUrl,
+                    method: "post",
+                    dataType: 'json',
+                    success: (res) => {
 
-                if(res['results'][i].content) {
-                    htmlToAppend += `
-                    <h3>Content:</h2>
-                    <p>${res['results'][i].content}</p>
-                    `
-                }
+                        if (res['results'].length == 0) {
+                            resHead.innerHTML = "Records Are Not Found Related To " + topicField.value
+                        }
+                        else if(res['results'].length != 0){
+                            appendResults(res);
+                        }
+                    },
+                    error: (err) => {
+                        console.log(err);
+                    }
+                });
 
-                htmlToAppend += `
-                       
-                        <span>${res['results'][i].pubDate}</span>
-                    </div>
-                `;
             }
-            $("#all-news-div").append(htmlToAppend);
-            topicField.value = "";
-            countryField.value = "";
+            else {
+                appendResults(res);
+            }
+           
         },
         error: (err) => {
             console.log(err);
         }
     });
 })
+
+function appendResults(res) {
+    let htmlToAppend = "";
+
+    for (let i = 0; i < res['results'].length; i++) {
+        console.log(res['results'][i]);
+        htmlToAppend += `
+            <div class="news-wrapper">
+                <h2>${res['results'][i].title}</h2>
+                <h3>Link:</h2>
+                <a href="${res['results'][i].link}">Click here</a>`
+
+        if (res['results'][i].image_url) {
+            htmlToAppend += `
+                <img src="${res['results'][i].image_url}" alt="" srcset="">
+            `
+        }
+
+        if (res['results'][i].description) {
+            htmlToAppend += `
+                <h3>Description:</h2>
+                <p>${res['results'][i].description}</p>
+            `
+        }
+
+        if (res['results'][i].content) {
+            htmlToAppend += `
+            <h3>Content:</h2>
+            <p>${res['results'][i].content}</p>
+            `
+        }
+
+        htmlToAppend += `
+               
+                <span>${res['results'][i].pubDate}</span>
+            </div>
+        `;
+    }
+    $("#all-news-div").append(htmlToAppend);
+    topicField.value = "";
+    countryField.value = "";
+
+    $("html, body").animate({
+        scrollTop: $("#all-news-div").offset().top - 100
+    }, 500);
+}
